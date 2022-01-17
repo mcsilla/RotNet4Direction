d1d5c979641a90d25b5b9a1e665a291484ff7081
Csilla Majoros <csilla.majoros@arcanum.com>
2022-01-17 16:34:35 +0100

    Add config map

---
+++ config/train/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""__init__ module for configs. Register your config file here by adding it's
+entry in the CONFIG_MAP as shown.
+"""
+
+from config.train import colab_config
+
+CONFIG_MAP = {
+    'colab': colab_config.CONFIG,
+}
