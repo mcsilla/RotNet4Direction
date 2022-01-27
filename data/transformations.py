1d25d2b335cb792c852d7aa21d27db76234571ae
Csilla Majoros <csilla.majoros@arcanum.com>
2022-01-27 17:38:04 +0100

    Correct transformations

--- data/transformations.py
+++ data/transformations.py
@@ -1,29 +1,29 @@
 import tensorflow as tf
 
 
-def random_brightness(image, label):
+def random_brightness(image):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
-    return tf.image.stateless_random_brightness(image, max_delta=0.1, seed=seed), label
+    return tf.image.stateless_random_brightness(image, max_delta=0.1, seed=seed)
 
-def random_contrast(image, label):
+def random_contrast(image):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
-    return tf.image.stateless_random_contrast(image, lower=0.8, upper=2, seed=seed), label
+    return tf.image.stateless_random_contrast(image, lower=0.8, upper=2, seed=seed)
 
-def random_hue(image, label):
+def random_hue(image):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
-    return tf.image.stateless_random_hue(image, max_delta=0.2, seed=seed), label
+    return tf.image.stateless_random_hue(image, max_delta=0.2, seed=seed)
 
-def random_saturation(image, label):
+def random_saturation(image):
     seed = tf.random.uniform(
         [2], minval=0, maxval=10, dtype=tf.dtypes.int32, seed=None, name=None
     )
-    return tf.image.stateless_random_saturation(image, lower=0.2, upper=2.5, seed=seed), label
+    return tf.image.stateless_random_saturation(image, lower=0.2, upper=2.5, seed=seed)
 
 
 def make_random_transformation(image):
